33fe388474ccf996a297c2675ad3324e2b571d3e
lambocalrissian <gncelambfam@gmail.com>
2018-08-13 00:07:58 -0600

    Lesson 07 assignment in progress

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -2,7 +2,33 @@
 from django.contrib import admin # <- this is already there.
 from myblog.models import Post
 from myblog.models import Category
+from django.contrib import admin
 
-admin.site.register(Post)
-admin.site.register(Category)
+from myblog.models import Post
+from myblog.models import Category
+
+# Create a customized ModelAdminLinks to an external site. class for the Post and Category models
+class CategoryAdmin(admin.ModelAdmin):
+    # Impacting the category form
+
+    fields = ('name', 'description')
+    
+
+class CategoryInline(admin.TabularInline):
+    model = Post.categories.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    # this affects the add post form, not the list of posts
+
+    fields = ('title', 'text', 'author')
+    inlines = [
+        CategoryInline,
+    ]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+# admin.site.register(Post)
+# admin.site.register(Category)
 
