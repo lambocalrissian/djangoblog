9d9af71bb9b7a7d62e65d18742fd4748b0f4d95c
lambocalrissian <gncelambfam@gmail.com>
2018-08-18 19:44:33 -0600

    lesson08 rest addon

--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.template import RequestContext, loader
+from django.http import HttpResponse, Http404
 from myblog.models import Post
+
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from myblog.serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
 
 def stub_view(request, *args, **kwargs):
     body = "Stub View\n\n"
@@ -13,11 +33,13 @@ def stub_view(request, *args, **kwargs):
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
 
+
 def list_view(request):
     published = Post.objects.exclude(published_date__exact=None)
     posts = published.order_by('-published_date')
     context = {'posts': posts}
     return render(request, 'list.html', context)
+
 
 def detail_view(request, post_id):
     published = Post.objects.exclude(published_date__exact=None)
@@ -27,3 +49,8 @@ def detail_view(request, post_id):
         raise Http404
     context = {'post': post}
     return render(request, 'detail.html', context)
+
+
+
+
+
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,18 +13,33 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
+from django.urls import path
 from django.contrib import admin
 from django.contrib.auth import login, logout
+from django.conf.urls import url, include
+
+from myblog import views
+from rest_framework import routers
+
+# rest add-on
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
 
 urlpatterns = [path('admin/', admin.site.urls),
                path('', include('myblog.urls')),
+               #url(r'^accounts/', include('allauth.urls')),
                path('login/', login, {'template_name': 'login.html'}, name="login"),
                path('logout/', logout, {'next_page': '/'}, name="logout"),
+               url(r'^', include(router.urls)),
+               url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
                ]
 #
 # urlpatterns = [path('admin/', admin.site.urls),
 #                path('', include('myblog.urls'),)
 #                ]
+
+
 
 
